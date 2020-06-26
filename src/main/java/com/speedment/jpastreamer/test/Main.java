2f2efb280e6463c26f59f7d9c1920886ae0cfa2c
Per Minborg <minborg@speedment.com>
2020-06-26 10:17:03 +0200

    Add examples

--- src/main/java/com/speedment/jpastreamer/test/Main.java
+++ src/main/java/com/speedment/jpastreamer/test/Main.java
@@ -17,8 +17,31 @@ public class Main {
                 .build();
 
         jpaStreamer.stream(Film.class)
+                .forEach(System.out::println);
+
+
+        jpaStreamer.stream(Film.class)
                 .filter(Film$.length.between(100, 120))
                 .forEach(System.out::println);
+
+        jpaStreamer.stream(Film.class)
+                // Composing filters with "and"/"or"
+                .filter(Film$.rating.equal("G").or(Film$.length.greaterOrEqual(140)))
+                .forEach(System.out::println);
+
+
+        jpaStreamer.stream(Film.class)
+                .filter(Film$.rating.equal("G"))
+                .sorted(Film$.length.reversed().thenComparing(Film$.title.comparator()))
+                .skip(10)
+                .limit(5)
+                .forEach(System.out::println);
+
+        long count = jpaStreamer.stream(Film.class)
+                .filter(Film$.rating.equal("G"))
+                .count();
+
+        System.out.println("There are "+count+" films with rating G");
 
         jpaStreamer.close();
 
